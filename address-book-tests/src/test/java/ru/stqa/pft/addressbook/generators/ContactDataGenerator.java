15211fd3751fcbf862b26a80a765c4094d377325
Evgeniy Korneev <eukorneev@gmail.com>
2020-07-14 17:56:06 +0300

    ContactsGenerator

---
+++ address-book-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,63 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstName("TestFirstName" + i).withLastName("TestLastName" + i)
+              .withAddress("TestAddress" + i)
+              .withMobilePhone("12345" + i).withHomePhone("54321" + i).withWorkPhone("87655" + i)
+              .withEmail(i + "testmail@gmail.com").withEmail2(i + "testmail2@gmail.com")
+              .withEmail3(i + "testmail3@gmail.com"));
+    }
+    return contacts;
+  }
+  }
