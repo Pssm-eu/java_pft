1f4eab12b56e9568eee412a3c6b7b611f2099cd0
Evgeniy Korneev <eukorneev@gmail.com>
2020-07-16 14:33:20 +0300

    hibernate

--- address-book-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ address-book-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,48 +1,86 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
+@Entity
+@Table(name = "addressbook")
+
 public class ContactData {
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstname")
   private String firstName;
+
   @Expose
+  @Column(name = "lastname")
   private String lastName;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "address")
   private String address;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+
   @Expose
+  @Transient
   private String allPhones;
+
   @Expose
+  @Transient
   private String email;
+
   @Expose
+  @Transient
   private String email2;
+
   @Expose
+  @Transient
   private String email3;
+
   @Expose
+  @Transient
   private String allEmails;
+
   @Expose
+  @Transient
   private String group;
+
+  @Transient
   private String allMainInfo;
 
+  @Type(type = "text")
+  @Column(name = "photo")
+  private String photo;
+
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
-
-  private File photo;
-
 
   public ContactData withId(int id) {
     this.id = id;
